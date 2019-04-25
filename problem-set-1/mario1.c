b83d82235157b45a8afe18d326e8ecb45a48c955
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-25 19:24:39 -0400

    more manipulating

--- problem-set-1/mario1.c
+++ problem-set-1/mario1.c
@@ -1,27 +1,27 @@
 //First attempt at the mario problem, starting with the easier one and then will attempt the harder one
 
 #include <cs50.h>
+
 #include <stdio.h>
 
 int main(void) {
   int height;
-  do 
-  {
+  do {
     printf("Enter a height; \n");
-    scanf("%i", &height);
-    for (int i = 0; i < height + 1; i++) {
-    {
-      char str[] = ".";
-      printf("%s", str);
-      for (int j = 0; j < i; j++) {
+    scanf("%i", & height);
+    for (int i = 1; i < height + 1; i++) {
+      {
+        char str[] = ".";
+        printf("%s", str);
+        for (int j = 0; j < i; j++) {
 
-        printf("#");
+          printf("#");
+        }
+        printf("\n");
       }
-      printf("\n");
+
     }
 
-  }  
-      
   }
   while (height > 8 || height < 0);
   return height;
