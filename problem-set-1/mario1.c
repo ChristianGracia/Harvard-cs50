977971b4ab481a02b6ce85ea2863b81e9eeaa937
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-25 14:38:10 -0400

    got the mario function to print #s with a nested loop

--- problem-set-1/mario1.c
+++ problem-set-1/mario1.c
@@ -9,5 +9,17 @@ int main(void)
     printf("Enter a height; \n");
     scanf("%d", &height);
     printf("hello, %d\n", height);
+    
+    int i;
+    int j;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            printf("#");
+        } 
+        printf("\n");
+        
+    }
 
 }
