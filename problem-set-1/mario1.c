dca64a342b69efdd2a2c7f56ce2fef5cfa84d89f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-25 14:24:33 -0400

    init of mario problem with the user input of height functional

---
+++ problem-set-1/mario1.c
@@ -0,0 +1,13 @@
+//First attempt at the mario problem, starting with the easier one and then will attempt the harder one
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    printf("Enter a height; \n");
+    scanf("%d", &height);
+    printf("hello, %d\n", height);
+
+}
