9aa1de8b3ed6f5d1079ecb1a96d1cbf6a037c5f0
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-28 13:51:36 -0400

    changed to else if statements, almost passes all cs50 reqs

--- problem-set-1/cash.c
+++ problem-set-1/cash.c
@@ -5,53 +5,68 @@
 int main (void)
 {
     float owed;
+    int quarters;
+    int dimes;
+    int nickels;
+    int pennies;
+    int total;
+    int total1;
+    int total2;
+    int total3;
 
     do
     {
         owed = get_float("How much change is owed?\n");
-
         int cents = round(owed * 100);
 
+        if (cents > 25)
+        {
+        quarters = round(cents/25);
+        total = cents - (quarters * 25);
+
+        dimes = round(total/10);
+        total1 = total - (dimes * 10);
+
+        nickels = round(total1/5);
+        total2= total1 - (nickels * 5);
+
+        pennies = round(total2/1);
+        total3 = quarters + nickels + dimes + pennies;
+        printf("\nChange owed is: %i", total3);
 
 
-        if (cents > 25) 
+        }
+
+        else if( cents < 25)
+        {
+        dimes = round(total/10);
+        total1 = total - (dimes * 10);
+        nickels = round(total1/5);
+        total2= total1 - (nickels * 5);
+        pennies = round(total2/1);
+        total3 = quarters + nickels + dimes + pennies -1;
+        printf("\nChange owed is: %i", total3);
+        }
+
+        else if (cents < 10 )
         {
 
-            int quarters;
-            int dimes;
-            int nickels;
-            int pennies;
-            int total;
-            int total1;
-            int total2;
-            int total3;
-
-            quarters = round(cents/25);
-            total = cents - (quarters * 25);
-
-             if (total > 10) 
-             {
-                 dimes = round(total/10);
-
-                 total1 = total - (dimes * 10);
+        nickels = round(total1/5);
+        total2= total1 - (nickels * 5);
+        pennies = round(total2/1);
+        total3 = quarters + nickels + dimes + pennies -1;
+        printf("\nChange owed is: %i", total3);
 
 
-                 if (total1 > 5)
-                 {
-                     nickels = round(total1/5);
-                     total2= total1 - (nickels * 5);
-
-
-                    if (total2 > 1)
-                    {
-                        pennies = round(total2/1);
-                        total3 = quarters + nickels + dimes + pennies -1;
-                        printf("\nChange owed is: %i", total3);
-                        printf("quarters = %i \n dimes = %i \n nickels = %i \n pennies = %i", quarters, dimes, nickels, pennies);
-                    }
-                 }
-             }
         }
+        else if (cents < 5)
+        {
+
+        pennies = round(total2/1);
+        total3 = quarters + nickels + dimes + pennies -1;
+        printf("\nChange owed is: %i", total3);
+        }
+
     }
     while (owed < 0);
 }
