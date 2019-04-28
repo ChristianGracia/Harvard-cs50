9dba9fcd71d38d202ad1da8f8f57a71625fd0b70
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-27 20:57:05 -0400

    final commit of the mario problem, fully functional and styled according to cs50 rules

--- problem-set-1/mario1.c
+++ problem-set-1/mario1.c
@@ -1,27 +1,31 @@
-//First attempt at the mario problem, starting with the easier one and then will attempt the harder one
+//fully functional program that askes user for inputted height and returns a right aligned pyramid starting with 1 that increases in size up to specified height.
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 
+int main(void)
+{
+    int height;
 
-int main(void) {
-  int height;
-
-  do {
-    height = get_int("Enter a height: ");
-    if (height <= 8 && height > 0) {
-      for (int i = 1; i < height + 1; i++) {
+    do
+    {
+        height = get_int("Enter a height: ");
+        if (height <= 8 && height > 0)
         {
-          int m = height - i;
-          printf("%*s", m, "");
-          for (int j = 0; j < i; j++) {
-            printf("#");
-          }
-          printf("\n");
-        }
+            for (int i = 1; i < height + 1; i++)
+            {
+                {
+                    int m = height - i;
+                    printf("%*s", m, "");
+                    for (int j = 0; j < i; j++)
+                    {
+                        printf("#");
+                    }
+                    printf("\n");
+                }
 
-      }
+            }
+        }
     }
-  }
-  while (height > 8 || height <= 0);
+    while (height > 8 || height <= 0);
 }
