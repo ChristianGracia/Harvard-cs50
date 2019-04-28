672243724aaecf5182b598b16ea3906fc902d747
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-27 20:12:41 -0400

    mario problem seems functional!

--- problem-set-1/mario1.c
+++ problem-set-1/mario1.c
@@ -1,23 +1,24 @@
 //First attempt at the mario problem, starting with the easier one and then will attempt the harder one
 #include <cs50.h>
+
 #include <stdio.h>
+
+#include <string.h>
+
 
 int main(void) {
   int height;
+
   do {
     printf("Enter a height; \n");
     scanf("%i", & height);
 
-    if (height < 8 && height >0) {
+    if (height <= 8 && height > 0) {
       for (int i = 1; i < height + 1; i++) {
         {
-             for (int k = 0; k < height; k++){
-                 char space[] = ".";
-                 printf("%s", space);
-             }
-
+            int m = height - i + 1;
+            printf("%*s", m, "");
           for (int j = 0; j < i; j++) {
-
             printf("#");
           }
           printf("\n");
