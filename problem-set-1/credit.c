358f6086cecb0aacfc2a05fd51250afbf5820281
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-28 15:09:51 -0400

    program can now count inputted digits

--- problem-set-1/credit.c
+++ problem-set-1/credit.c
@@ -1,9 +1,37 @@
 //program checks if inputted credit card value is valid
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
+
+int numPlaces (long n) 
+{
+    if (n < 0) n = (n == LONG_MIN) ? LONG_MAX : -n;
+    if (n < 10) return 1;
+    if (n < 100) return 2;
+    if (n < 1000) return 3;
+    if (n < 10000) return 4;
+    if (n < 100000) return 5;
+    if (n < 1000000) return 6;
+    if (n < 10000000) return 7;
+    if (n < 100000000) return 8;
+    if (n < 1000000000) return 9;
+    if (n < 10000000000) return 10;
+    if (n < 100000000000) return 11;
+    if (n < 1000000000000) return 12;
+    if (n < 10000000000000) return 13;
+    if (n < 100000000000000) return 14;
+    if (n < 1000000000000000) return 15;
+    if (n < 10000000000000000) return 16;
+    if (n < 100000000000000000) return 17;
+
+    return false;
+
+}
 
 int main(void){
-    long credit = get_long("Enter Credit Card Number: ");
-    printf("\n%li", credit);
-    
+       long credit = get_long("Enter Credit Card Number: ");
+       printf("\n%li",  credit);
+       long n = credit;
+       int digits = numPlaces(credit);
+       printf("\n%i", digits);
 }
