faa9e9b5a6e84d77afa947376971cdbb991b618e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-30 21:42:03 -0400

    credit card check program fully functional, final commit

--- problem-set-1/credit.c
+++ problem-set-1/credit.c
@@ -1,88 +1,7 @@
-//program checks if inputted credit card value is valid
 #include <stdio.h>
 #include <cs50.h>
 #include <math.h>
 #include <stdbool.h>
-
-int numPlaces(long n)
-{
-    if (n < 0)
-    {
-        n = (n == LONG_MIN) ? LONG_MAX : -n;
-    }
-    if (n < 10)
-    {
-        return 1;
-    }
-
-    if (n < 100)
-    {
-        return 2;
-    }
-    if (n < 1000)
-    {
-        return 3;
-    }
-    if (n < 10000)
-    {
-        return 4;
-    }
-    if (n < 100000)
-    {
-        return 5;
-    }
-    if (n < 1000000)
-    {
-        return 6;
-    }
-    if (n < 10000000)
-    {
-        return 7;
-    }
-    if (n < 100000000)
-    {
-        return 8;
-    }
-    if (n < 1000000000)
-    {
-        return 9;
-    }
-    if (n < 10000000000)
-    {
-        return 10;
-    }
-    if (n < 100000000000)
-    {
-        return 11;
-    }
-    if (n < 1000000000000)
-    {
-        return 12;
-    }
-    if (n < 10000000000000)
-    {
-        return 13;
-    }
-    if (n < 100000000000000)
-    {
-        return 14;
-    }
-    if (n < 1000000000000000)
-    {
-        return 15;
-    }
-    if (n < 10000000000000000)
-    {
-        return 16;
-    }
-    if (n < 100000000000000000)
-    {
-        return 17;
-    }
-
-    return false;
-
-}
 
 int firstDigit(int n)
 {
@@ -94,37 +13,99 @@ int firstDigit(int n)
     }
     return n;
 }
-
-bool checkSum(credit)
+int first2Digit(int n)
 {
-    return true;
+    while
+    (n >= 100)
+    {
+        n /= 10;
+    }
+    return n;
+
+}
+long long countCheck(long long num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        num = num / 10;
+        count = count + 1;
+    }
+    return count;
 }
 
 int main(void)
 {
-
-
     long long credit = get_long_long("Enter Credit Card Number: ");
     long long n = credit;
-    int digits = numPlaces(credit);
-    int first = firstDigit(credit / 10000000);
-    bool check = checkSum(credit);
+    int first = firstDigit(credit / 1000000000);
+    long long digits = countCheck(credit) + 1;
+    long long cardDigits[digits];
+    int first2 = first2Digit(credit / 100000000);
 
-    if (first == 3 && check == true)
+    for (int i = (digits - 1); i >= 0; i--)
     {
-        printf("AMEX\n");
-    }
-    if (first == 5 && check == true)
-    {
-        printf("MASTERCARD\n");
-    }
-    if (first == 4 && check == true)
-    {
-        printf("VISA\n");
+        cardDigits[i] = credit % 10;
+        credit = floor(credit / 10);
     }
 
-    else if (digits > 16 || digits < 12)
+
+    long long doubleOdd[digits];
+
+
+    for (int j = (digits - 2), l = 0; j >= 0; j -= 2, l++)
+    {
+        doubleOdd[l] = cardDigits[j] * 2;
+    }
+
+    int sum = 0;
+    for (int m = 0; m <= ((digits - 2) / 2); m++)
+    {
+        if (doubleOdd[m] > 9)
+        {
+            sum = sum + ((floor(doubleOdd[m] / 10)) + (doubleOdd[m] % 10));
+        }
+        else
+        {
+
+            sum = sum + doubleOdd[m];
+        }
+    }
+
+    int sumEven = 0;
+
+    for (int k = digits - 1; k >= 0 ; k -= 2)
+    {
+        int evenDigit = cardDigits[k];
+        sumEven = sumEven + evenDigit;
+    }
+
+    int check = (sumEven + sum) % 10;
+
+
+    if (digits > 17 || digits < 12 || check >= 1)
     {
         printf("INVALID\n");
     }
+
+    else if (check < 1)
+    {
+        if (first2 == 37 || first2 == 34)
+        {
+            printf("AMEX\n");
+        }
+        if (first2 == 51 || first2 == 51 || first2 == 52 || first2 == 53 || first2 == 54 || first2 == 55)
+        {
+            printf("MASTERCARD\n");
+        }
+        if (first == 4)
+        {
+            printf("VISA\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
+    }
+
 }
