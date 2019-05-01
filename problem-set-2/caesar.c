1ed342f071dedc22f8e10af164a654c7154ef3a4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-05-01 18:12:56 -0400

    added a function that checks if argc > 1 so a segmentation fault does not occur when looping through argv if theres no argument

--- problem-set-2/caesar.c
+++ problem-set-2/caesar.c
@@ -4,22 +4,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+int checkValid(string, int);
+int argExists(int);
 
 int main(int argc, string argv[])
 {
-    string key =  argv[1];
-    int valid = 0;
+    string key = argv[1];
+    int argExists1 = argExists(argc);
+    int valid = checkValid(key, argc);
 
-    for (int i = 0; i < strlen(key); i++){
-          if (isdigit(key[i]) == false){
-            return 0;
-        }
-        else {
-            valid = 1;
-        }
-    }
-
-    if (argc != 2 || key < 0 || valid < 1)
+    if (argc != 2 || key < 0 || valid < 1 || argExists1 < 1)
     {
         printf("Usage: ./caesar key\n");
         return 1;
@@ -30,10 +26,38 @@ int main(int argc, string argv[])
         printf("Sucess\n");
         printf("%s\n", key);
         string text = get_string("input text to be encrypted: ");
-        int textToNumber = atoi(text);
+        int textToNumber[strlen(text)];
+        for (int i = 0; i < strlen(text); i++)
+        {
+            textToNumber[i] += atoi(text);
+            printf("%d", textToNumber[i]);
 
-        printf("%i\n", textToNumber);
+        }
     }
+}
 
-
+int argExists (int c){
+    if (c > 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int checkValid(string n, int b)
+{
+    if (b > 1)
+    {
+        for (int i = 0; i < strlen(n); i++)
+        {
+        if (isdigit(n[i]) == false)
+        {
+            return 0;
+            break;
+        }
+    }
+    }
+    return 1;
 }
