9ad4485322febb957a702ede9323500f7a10d150
Christian Gracia <christiangracia.student@careerdevs.com>
2019-05-02 00:04:34 -0400

    function for getting value of shift based on upper and lower case letters is functional

--- problem-set-2/vigenere.c
+++ problem-set-2/vigenere.c
@@ -1,16 +1,20 @@
 #include <cs50.h>
+#include <cs50.h>
 #include <ctype.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 int argExists(int);
 int checkValid(string, int);
+int shift(char c);
 
 int main(int argc, string argv[])
 {
     string key = argv[1];
     int argExists1 = argExists(argc);
     int valid = checkValid(key, argc);
+
 
     if (argc != 2 || argExists1 < 1 || valid < 1 )
     {
@@ -19,7 +23,44 @@ int main(int argc, string argv[])
     }
     else
     {
+        printf("Sucess\n");
+        printf("%s\n", key);
+
+        //get string
         string text = get_string("Enter text here: ");
+        int length = strlen(text);
+        printf("plaintext: %s\n", text);
+
+
+        char key1 = key[0];
+
+        int keynum = shift(key1);
+
+
+        printf("keynum is: %i\n", keynum);
+
+        // for (int i = 0; i <= length; i++){
+
+        //     if (isalpha(text[i]) == true)
+        //     {
+        //         printf("\n%c", text[i] + keynum);
+        //     }
+            // else {
+            //     printf("%c\n", text[i]);
+            // }
+
+        // }
+
+    }
+}
+
+int shift(char c){
+    if (c > 96){
+        return (c - 97);
+    }
+    else
+    {
+        return (c - 65);
     }
 }
 
