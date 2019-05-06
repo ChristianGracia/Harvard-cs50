2e1cec10f05a83a76a987cba6a2efadf4abde578
Christian Gracia <christiangracia.student@careerdevs.com>
2019-05-06 18:30:19 -0400

    init of resize.c, program resizes an image by a inputted factor

---
+++ problem-set-3/resize/less/resize.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (int argc, string argv[])
+{
+    if (argc != 3){
+        printf("Usage: ./resize n infile outfile\n");
+        return 1;
+    }
+    else
+    {
+
+    }
+
+}
