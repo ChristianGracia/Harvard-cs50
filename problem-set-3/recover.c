f0ffa2e730244b63bedf1f8c0917267325a7858b
Christian Gracia <christiangracia.student@careerdevs.com>
2019-05-31 17:24:15 -0400

    recover program init

---
+++ problem-set-3/recover.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+   if (argc = 2)
+   {
+
+   }
+   else
+   {
+       printf("Usage: ./recover image");
+       return 0;
+   }
+}
