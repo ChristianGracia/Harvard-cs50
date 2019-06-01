c30a83ec6fa0d2fae63fe9d10e63f741539aed3d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-01 10:54:41 -0400

    added fopen, fread, and fwrite for jpegs

--- problem-set-3/recover.c
+++ problem-set-3/recover.c
@@ -5,11 +5,20 @@ int main(int argc, char *argv[])
 {
    if (argc = 2)
    {
+       FILE *fopen(argv[1], "r");
+
+
+       for (int i = 0; i < 20; i++)
+       {
+           fread(&argv[1], 512, 1, inptr);
+           fwrite(900 + i".jpeg", 512, 1, inptr);
+       }
+
 
    }
    else
    {
        printf("Usage: ./recover image");
-       return 0;
+       return 1;
    }
 }
