1401405d1e87e5c48db18e58f262f1728e2222c0
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-03 18:29:49 -0400

    file is being read and errors are set up for incorrect file usuage or incorrectly named data to be recovered

--- problem-set-3/recover.c
+++ problem-set-3/recover.c
@@ -3,17 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-   if (argc = 2)
+   if (argc == 2)
    {
-       FILE *fopen(argv[1], "r");
-
-
-       for (int i = 0; i < 20; i++)
+       FILE *inptr = fopen(argv[1], "r");
+       if (inptr == NULL)
        {
-           fread(&argv[1], 512, 1, inptr);
-           fwrite();
+            fprintf(stderr, "Could not open %s.\n", argv[1]);
+            return 2;
        }
+       else
+       {
+           printf("file being read");
 
+       }
 
    }
    else
