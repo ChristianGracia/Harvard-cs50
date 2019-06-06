b00d2b0f9a50e343764edc4443518e65b8eac57f
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-06 17:17:17 -0400

    small fixes

--- problem-set-3/recover.c
+++ problem-set-3/recover.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
        }
        else
        {
-
            printf("file being read");
            for(int i = 0; i < 20; i++)
            {
-              buffer[512]
+              buffer[512];
               fread(buffer, 512, 1, inptr);
-              if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 == 0xe0)){
+              if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 == 0xe0))
+              {
                  sprintf(filename, "%03i", i);
                  FILE *img= fopen(filename, "w");
               }
