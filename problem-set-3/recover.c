088226795054fc4901d6bac3985549ea1187d4cb
Christian Gracia <christiangracia.student@careerdevs.com>
2019-06-12 17:20:47 -0400

    restarting

--- problem-set-3/recover.c
+++ problem-set-3/recover.c
@@ -3,35 +3,31 @@
 
 int main(int argc, char *argv[])
 {
-   if (argc == 2)
-   {
-       FILE *inptr = fopen(argv[1], "r");
-       if (inptr == NULL)
-       {
-            fprintf(stderr, "Could not open %s.\n", argv[1]);
-            return 2;
-       }
-       else
-       {
-           printf("file being read");
-           for(int i = 0; i < 20; i++)
-           {
-              buffer[512];
-              fread(buffer, 512, 1, inptr);
-              if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0 == 0xe0))
-              {
-                 sprintf(filename, "%03i", i);
-                 FILE *img= fopen(filename, "w");
-              }
-           }
-       } 
-       fclose(img);
-   }
-   else
-   {
-       fprintf(stderr, "Usage: ./recover image");
-       return 1;
-   }
-   return 0;
-}
 
+    //check for correct usage
+    if (argc != 2)
+    {
+        fprintf(stderr, "Could not open %s. Please only use one argument.\n", argv[1]);
+        return 1;
+    }
+
+    //open file to be read
+    FILE *inptr = fopen(argv[1], "r");
+
+    //checks if file exists
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s. Incorrect file name.\n", argv[1]);
+        return 2;
+    }
+
+    //create output file
+    FILE *outptr = NULL;
+    
+    //create buffer for image data to be collected
+    unsigned char buffer[512];
+    
+    
+
+
+}
