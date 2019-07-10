36aa020fe50ed7584a75449bcd6abc3c2d41b622
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-09 20:47:56 -0400

    update mario is printing with a nested loop but not the amount of height yet

--- problem-set-6/mario.py
+++ problem-set-6/mario.py
@@ -2,9 +2,44 @@ i = 0
 while i < 1:
     userheight = int(input("height:"))
     if userheight > 0:
-        print ("hi")
-        i = 1
+        if userheight < 9:
+            print ("hi")
+            i = 1
+        else:
+            print("fail")
+            i = 0
     else:
         print("fail")
         i = 0
 print (userheight)
+height = userheight
+
+k=height
+while (k>0):
+    print( "#" +(""*k)+ "#")
+    k=k-1
+    
+    
+    
+# for x in range(0, userheight):
+#     for y in range (0, x):
+#         print ("#") * y
+    
+    
+    
+    # print ("#")
+
+
+
+
+
+# for x in range(0, height):
+#     print ("#")
+
+# if height > 0:
+#     print('too low')
+# elif 0 < height < 9:
+#     print("#")
+# else:
+#     print('too high')
+
