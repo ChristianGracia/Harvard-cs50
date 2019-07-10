266c0f03cc8b797296d972a807632892d855b9dc
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-10 14:46:07 -0400

    update, trying to get luhns algorithm check work

--- problem-set-6/credit.py
+++ problem-set-6/credit.py
@@ -10,3 +10,27 @@ while (numcheck > 10):
 
 digitCount += 1
 print(digitCount)
+
+numberRead = int(number)
+
+sum = 0
+secondOrFirst = False
+
+for x in range (0, digitCount - 1):
+    d = numberRead / (10 * digitCount - 1)
+    
+    if secondOrFirst == True:
+        d = d * 2
+        sum += d/ 10
+        sum += d % 10
+        if secondOrFirst == True:
+            secondOrFirst = False
+        else:
+            secondOrFirst = True
+            
+    if sum == 0:
+        secondOrFirst = True
+    else:
+        secondOrFirst = False
+
+print(secondOrFirst)
