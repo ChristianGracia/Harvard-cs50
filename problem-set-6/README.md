f5c99b4774c9aecdb69d119fb82d352550176822
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-09 22:35:52 -0400

    mario more complete

--- problem-set-6/README.md
+++ problem-set-6/README.md
@@ -3,5 +3,6 @@ Hello, mario, cash/credit, caesar/vigenere, bleep in Python
 
 ## Complete
 
--hello **[x]**
--mario (less) **[x]**
+-hello -- **[x]**
+-mario (less) -- **[x]**
+-mario (more) -- ** [x]**
---
+++ problem-set-6/mario-more.py
@@ -0,0 +1,27 @@
+i = 0
+while i < 1:
+    userheight = input("height:")
+    
+    if userheight.isalpha():
+        i = 0
+    elif userheight.isdigit():
+        height = int(userheight)
+    
+        if height > 0:
+            if height < 9:
+                i = 1
+            else:
+                i = 0
+        else:
+            i = 0
+
+counter = height
+hashes = 1
+spaces = height-1
+
+for x in range(0, height):
+    while (counter>0):
+        print ((" "*spaces)+("#"*hashes) + "  " + ("#"*hashes))
+        spaces= spaces-1
+        hashes = hashes + 1
+        counter = counter-1
