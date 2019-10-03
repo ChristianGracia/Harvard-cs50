d989594e1fa56121be78b5a2d4952e220e7cd20d
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-02 21:48:46 -0400

    letters being converted to numbers

--- problem-set-6/caesar.py
+++ problem-set-6/caesar.py
@@ -8,7 +8,12 @@ key = int(sys.argv[1])
 while len(inputString) == 0:
     inputString = cs50.get_string("input a string to be encrypted\n")
 
+stringLength = len(inputString)
+
 while key > 26:
     key -= 26
 
 print("plaintext: " + inputString)
+
+for letter in inputString:
+    print(ord(letter))
