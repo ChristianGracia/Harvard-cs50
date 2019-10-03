79166a1fa9cb7b7a0a8c6268b3d0761e7a7e1d35
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-03 00:24:58 -0400

    getting user input

--- problem-set-6/bleep/bleep.py
+++ problem-set-6/bleep/bleep.py
@@ -15,7 +15,13 @@ def main():
     bannedWords = set()
     for x in range((len(contents) - 1)):
         bannedWords.add((contents[x].strip()))
-    print(bannedWords)
+    userInput = get_string("What message would you like to censor?\n")
+
+    inputWords = userInput.split(" ")
+    print(inputWords)
+
+    for x in range(len(inputWords)):
+        print(inputWords[x])
 
 
 if __name__ == "__main__":
