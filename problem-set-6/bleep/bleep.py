98780a5241dfef2e518996ebdc203ac4e4bbbb29
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-03 00:13:22 -0400

    banned words are added to the set and cleaned up

--- problem-set-6/bleep/bleep.py
+++ problem-set-6/bleep/bleep.py
@@ -3,14 +3,19 @@ from sys import argv
 
 
 def main():
+
     if len(argv) != 2:
         print("Usage: python bleep.py dictionary")
-        return 0
+        return 1
 
     f = open(argv[1], "r")
     if f.mode == 'r':
-        contents = f.read()
-        print(contents)
+        contents = f.readlines()
+
+    bannedWords = set()
+    for x in range((len(contents) - 1)):
+        bannedWords.add((contents[x].strip()))
+    print(bannedWords)
 
 
 if __name__ == "__main__":
