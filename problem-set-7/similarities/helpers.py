0847f5b38372305ad3b3d3544d6be5faa3e7b37e
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-04 18:33:04 -0500

    finishing up python portion

--- problem-set-7/similarities/helpers.py
+++ problem-set-7/similarities/helpers.py
@@ -1,8 +1,34 @@
+from nltk.tokenize import sent_tokenize
+import re
+
 def lines(a, b):
     """Return lines in both a and b"""
 
-    # TODO
-    return []
+    linesA = a.split("\n")
+    linesB = b.split("\n")
+    linesMatch = []
+
+    counter = 0
+    while counter < len(linesA):
+        flag = True
+        for x in linesA:
+            for y in linesB:
+                if x != y:
+                    flag = False
+        if flag == True:
+            linesMatch.append(linesA)
+
+        counter += 1
+
+    for c in linesA:
+      print(c);
+    for c in linesB:
+      print(c);
+    for c in linesMatch:
+       print(c);
+
+
+    return linesMatch
 
 
 def sentences(a, b):
