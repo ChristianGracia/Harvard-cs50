fc1dfbdda580632bf8546dda430945c1d73436a3
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-04 23:36:19 -0500

    similaries completed

--- problem-set-7/similarities/helpers.py
+++ problem-set-7/similarities/helpers.py
@@ -1,6 +1,7 @@
 from nltk.tokenize import sent_tokenize
 import re
 
+
 def lines(a, b):
 
     linesA = a.split("\n")
@@ -8,21 +9,19 @@ def lines(a, b):
     linesB = b.split("\n")
     linesMatch = []
 
-
     for x in linesA:
         for y in linesB:
             if x == y:
                 if y not in linesMatch:
                     linesMatch.append(y)
 
-
     return linesMatch
 
 
 def sentences(a, b):
 
-    aList = sent_tokenize(a);
-    bList = sent_tokenize(b);
+    aList = sent_tokenize(a)
+    bList = sent_tokenize(b)
     sentanceMatch = []
 
     for x in aList:
@@ -31,27 +30,19 @@ def sentences(a, b):
                 if y not in sentanceMatch:
                     sentanceMatch.append(y)
 
-
     return sentanceMatch
 
 
 def substrings(a, b, n):
 
+    counter = 0
+
     stringMatch = []
 
-    for x in range(0, len(a)):
-        subStringA = a[x:x+n]
-
-        if subStringA in b:
-            if subStringB not in a:
-                stringMatch.append(subStringA)
-        for y in range(0, len(b)):
-            subStringB = b[y:y+n]
-            if subStringB in a:
-                if subStringB not in a:
-                    stringMatch.append(subStringB)
-            if subStringA == subStringB:
-                if subStringB not in stringMatch:
-                    stringMatch.append(subStringB)
+    while counter < len(a) - n + 1:
+        if a[counter:counter+n] in b:
+            if a[counter:counter+n] not in stringMatch:
+                stringMatch.append(a[counter:counter+n])
+        counter += 1
 
     return stringMatch
