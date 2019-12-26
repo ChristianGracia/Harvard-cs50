449cc2ae5a1e42122e62c5e6ed7fac2bcdbdffac
Christian Gracia <christiangracia.student@careerdevs.com>
2019-12-25 23:49:32 -0500

    changed response codes from invalid user input during registration from 403 to 400

--- problem-set-8/finance/application.py
+++ problem-set-8/finance/application.py
@@ -128,11 +128,11 @@ def register():
     if request.method == "POST":
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
         elif request.form.get("password") != request.form.get("confirmation"):
             return apology("Passwords must match", 403)
 
